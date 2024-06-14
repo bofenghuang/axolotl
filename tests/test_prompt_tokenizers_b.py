441aa771c8118f98497d11a269102d5c729d1959
bofenghuang <bofenghuang7@gmail.com>
2024-06-14 15:14:00 +0000

    Add phi3 tokenizer test

--- tests/test_prompt_tokenizers_b.py
+++ tests/test_prompt_tokenizers_b.py
@@ -181,3 +181,53 @@ class TestPromptTokenizationStrategiesVigogneChatV4(unittest.TestCase):
         )
         assert decode_labels(mt_labels) == """<|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>ipsum<|end_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>sit<|end_of_text|>"""
         # fmt: on
+
+
+class TestPromptTokenizationStrategiesVigogneChatV4Phi3(unittest.TestCase):
+    """
+    Test class for prompt tokenization strategies.
+    """
+
+    def setUp(self) -> None:
+        # pylint: disable=duplicate-code
+        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-small-128k-instruct", trust_remote_code=True)
+        self._print_token_id = self.tokenizer.bos_token_id
+
+    def test_sharegpt_llama_3(self):
+        "Make sure the sharegpt/llama is tokenized and formatted correctly."
+        strat = prompt_strat("vigogne_chat_v4_phi3", self.tokenizer)
+
+        def tokenize(conv):
+            result = strat.tokenize_prompt(deepcopy(conv))
+            return result["input_ids"], result["labels"]
+
+        def decode(ids):
+            return strat.tokenizer.decode(ids)
+
+        def decode_labels(ids):
+            return decode(list(map(lambda x: self._print_token_id if x == -100 else x, ids)))
+
+        # fmt: off
+        # No system message, single-turn
+        ns_input_ids, ns_labels = tokenize(test_data["single_turn_no_sys"])
+        assert decode(ns_input_ids) == """<|begin_of_text|><|user|>abc<|end_of_text|><|assistant|>ipsum<|end_of_text|>"""
+        assert decode_labels(ns_labels) == """<|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>ipsum<|end_of_text|>"""
+        # assert ns_input_ids == [1, 518, 25580, 29962, 25638, 518, 29914, 25580, 29962, 23421, 2]
+
+        # System message, single-turn conversations
+        st_input_ids, st_labels = tokenize(test_data["single_turn_sys"])
+        assert (
+            decode(st_input_ids)
+            == """<|begin_of_text|><|system|>lorem<|end_of_text|><|user|>abc<|end_of_text|><|assistant|>ipsum<|end_of_text|>"""
+        )
+        assert decode_labels(st_labels) == """<|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>ipsum<|end_of_text|>"""
+
+        # System message, multi-turn conversations
+        mt_input_ids, mt_labels = tokenize(test_data["multi_turn_sys"])
+        assert (
+            decode(mt_input_ids)
+            == """<|begin_of_text|><|system|>lorem<|end_of_text|><|user|>abc<|end_of_text|><|assistant|>ipsum<|end_of_text|><|user|>123<|end_of_text|><|assistant|>sit<|end_of_text|>"""
+        )
+        assert decode_labels(mt_labels) == """<|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>ipsum<|end_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|><|begin_of_text|>sit<|end_of_text|>"""
+        # fmt: on
+
